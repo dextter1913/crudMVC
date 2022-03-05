8b9d80b021ef4ad02f6c265fe0e4993a7ee3c6e0
dextter1913 <dextter1913@gmail.com>
2022-03-05 17:08:42 -0500

    Agregando views y agregando autoload

---
+++ app/controller/controller.php
@@ -0,0 +1,11 @@
+<?php 
+    require_once 'app/models/autoload.php';
+    
+    class controller
+    {
+        public function inicio()
+        {
+
+        }
+    }
+?>
---
+++ app/models/autoload.php
@@ -0,0 +1,7 @@
+<?php 
+function app_autoload($clases){
+    require_once 'app/models/clases/'.$clases.'.php';
+}
+
+spl_autoload_register('app_autoload');
+?>
